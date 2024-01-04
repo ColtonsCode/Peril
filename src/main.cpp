26d51f082443211b23248ea7b26d028e6c424cca
ColtonsCode <125777669+ColtonsCode@users.noreply.github.com>
2024-01-04 11:53:52 -0600

    Add and set window icon for fun

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,16 @@
 #include <raylib.h>
+#include <string>
+
+std::string resourcePath = "../../resources/";
+
 
 int main() {
     InitWindow(800, 600, "Peril");
+
+    // Icon
+    Image icon = LoadImage((resourcePath + "icon.png").c_str());
+    SetWindowIcon(icon);
+
     while (!WindowShouldClose()) {
         BeginDrawing();
         ClearBackground(RAYWHITE);
